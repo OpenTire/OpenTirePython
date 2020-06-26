6652c49031071f70cba9080a2837fdd95b7317a7
Henning Olsson <Henning.Olsson@calspan.com>
2020-06-26 10:11:15 -0600

    Update setup.py
    Changed typo in description

--- code/setup.py
+++ code/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='OpenTire',
     version='0.1',
-    description='An open-source mathematical tire model library for tire and vehicle research and development.',
+    description='An open-source mathematical tire model libraries for tire and vehicle research and development.',
     url='https://github.com/OpenTire/OpenTire',
     author='OpenTire',
     license='MIT',
